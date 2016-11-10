8f02e4e0e0697187f0fec729cf9303be0dbe2265
nkscoder <nkscoder@gmail.com>
2016-11-10 19:04:45 +0530

    update contact number

--- Mail.php
+++ Mail.php
@@ -87,7 +87,7 @@ $headers = "MIME-Version: 1.0" . "\r\n";
 $headers = "From:hi@scaledesk.com\r\n";
 $headers = "Content-type: text/html;charset=iso-8859-1" . "\r\n";
 
-
+$phone=$_POST['phone'];
 
 $message ='<html>
 <body>
